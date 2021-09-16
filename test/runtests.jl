640d3fc84f16de7c9cf7d054c9ce33ffe2ab18fd
Long Wang <wangl.cc@outlook.com>
2021-09-17 01:15:28 +0800

    test: add `Aqua`

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using ArrayInterface
 using ResizingTools
 using ResizingTools: setindex
+using Aqua
 
 const V = rand(3)
 const M = rand(3, 3)
@@ -23,8 +24,26 @@ const ITRS = (
 )
 
 _tolen(itr) = eltype(itr) <: Bool ? sum(itr) : length(itr)
+_params(T::UnionAll) = _params(T.body)
+_params(T::DataType) = T.parameters
 
 @testset "ResizingTools" begin
+    @testset "QA" begin
+        @testset "Ambiguity" begin
+            ambiguities = Test.detect_ambiguities(ResizingTools)
+            filter!(ambiguities) do (m1, m2)
+                p1 = _params(m1.sig)
+                p2 = _params(m2.sig)
+                for (t1, t2) in zip(p1, p2)
+                    typeintersect(t1, t2) === Union{} || return false
+                end
+                return true
+            end
+            @test length(ambiguities) == 0
+        end
+        Aqua.test_all(ResizingTools; ambiguities=false)
+    end
+
     @testset "arraymath" begin
         include("arraymath.jl")
     end
